656205620eaba2933a9b645c9a84083c64b4b1a6
twodzinski@op.pl <twodzinski@op.pl>
2023-08-24 23:13:44 +0200

    beforeAll, afterAll

--- src/main/java/pl/tomwodz/testing/Order.java
+++ src/main/java/pl/tomwodz/testing/Order.java
@@ -22,4 +22,10 @@ public class Order {
         this.meals.clear();
     }
 
+    @Override
+    public String toString() {
+        return "Order{" +
+                "meals=" + meals +
+                '}';
+    }
 }
