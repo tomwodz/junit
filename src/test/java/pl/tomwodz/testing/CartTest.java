41deb37cec6e5a13b4ee374042c12d2cbf8c87c6
twodzinski@op.pl <twodzinski@op.pl>
2023-08-24 23:24:43 +0200

    Disabled, displayName

--- src/test/java/pl/tomwodz/testing/CartTest.java
+++ src/test/java/pl/tomwodz/testing/CartTest.java
@@ -1,14 +1,19 @@
 package pl.tomwodz.testing;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test cases for Cart")
 class CartTest {
 
+    //@Disabled
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 100 ms")
     void simulateLargeOrder() {
 
         //given
