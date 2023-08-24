235d6522ee53ea2e9cc22010830ee7bcc967fcb9
twodzinski@op.pl <twodzinski@op.pl>
2023-08-24 23:20:43 +0200

    assertTimeout

---
+++ src/test/java/pl/tomwodz/testing/CartTest.java
@@ -0,0 +1,21 @@
+package pl.tomwodz.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+
+        //given
+        Cart cart = new Cart();
+
+        //when
+        //then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+}
