90e1c69e911c6b29cd050325914ad823ce30f9ef
twodzinski@op.pl <twodzinski@op.pl>
2023-08-25 09:59:04 +0200

    testFactory, dynamicTest

--- src/main/java/pl/tomwodz/testing/Meal.java
+++ src/main/java/pl/tomwodz/testing/Meal.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Meal {
     private int price;
+    private int quantity;
     private String name;
 
     public Meal(int price) {
@@ -12,6 +13,12 @@ public class Meal {
 
     public Meal(int price, String name) {
         this.price = price;
+        this.name = name;
+    }
+
+    public Meal(int price, int quantity, String name) {
+        this.price = price;
+        this.quantity = quantity;
         this.name = name;
     }
 
@@ -24,6 +31,10 @@ public class Meal {
         }
 
         return this.price - discount;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     @Override
